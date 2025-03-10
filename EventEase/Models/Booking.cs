83baf73129e13b8e3244fa7a96accfe6c58b3f48
User <User@DESKTOP-GAB3BFD>
2025-03-10 03:52:37 +0200

    created remaining models and changed headings

---
+++ EventEase/Models/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Models
+{
+    public class Booking
+    {
+
+        [Key] public int BookingId { get; set; }
+
+        public int EventId { get; set; }
+
+        public int VenueId { get; set; }
+
+    
+        [DataType(DataType.Date)]
+        public DateTime BookingDate { get; set; }
+
+    }
+}
+
